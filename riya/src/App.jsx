16a4312165e9838e51b5e0ddb4786931e67755de
riyaverma85 <riyaverma2976@gmail.com>
2025-11-21 23:46:18 -0800

    ..

--- riya/src/App.jsx
+++ riya/src/App.jsx
@@ -1,16 +1,50 @@
 //rafce
-
-
- const App = () => {
- const Riya=()=>{
-
-  for(let i=1;i<=5;i++){
-    console.log(i)
-  }
+import React, { useState } from 'react'
+import Navbar from './assets/Navbar';
+import Header from './assets/footer';
+import Img from'./assets/react.svg'
+import ClickMeButton from './assets/footer';
+const App = () => {
+let age=18;
+//  const[age,setAge]=useState("");
+ const loadAge=()=>{
+    if(age>=18){
+      alert(" vote")
+    }
+    else{
+      alert("note vote")
+    }
  }
+//  const App = () => {
+//  const Riya=()=>{
+// let sum=0
+//   for(let i=1;i<=10;i++){
+    
+//     if(i%2==0){
+//       sum++;
+//     } 
+//   }
+//   alert(`total even numbers =${sum}`)
+//  }
+//   return (
+//     <div>
+//       <button onClick={Riya} >count even Numbers</button><br/><br/>
+//     </div>
+//   )
+// }
+// export default App;
+
+//////////////////////location .href="form"dekhna he
   return (
     <div>
-      <button onClick={Riya} >Show Numbers</button>
+    <Navbar/>
+      <h1>hello app</h1>
+      {/* <input type="text" value={age} onChange={(e)=>setAge(e.target.value)}/> */}
+      <button onClick={loadAge} style={{color:"red"}}>Click me</button><br/><br/>
+    <img src={Img} alt="" />
+    <Header/>
+    <ClickMeButton/>
+    {/* <Footer2/> */}
     </div>
   )
 }
