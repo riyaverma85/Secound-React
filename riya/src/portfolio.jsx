7a2726dab8c0cdfd37d60bb0da55abf42a831f26
riyaverma85 <riyaverma2976@gmail.com>
2025-11-26 23:20:26 -0800

    .

--- riya/src/portfolio.jsx
+++ riya/src/portfolio.jsx
@@ -3,7 +3,7 @@ import "./portfolio.css"
 const portfolio = () => {
   return (
     <>
-    <header className='flex gap-40  border-1 bg-blue-500'>
+    <header className='flex gap-40  border-1 bg-fuchsia-900'>
       <i className='ml-2'> My portfolio</i>  
 <ul className='flex gap-6'>
     <li>Home</li>
@@ -13,7 +13,9 @@ const portfolio = () => {
     <li>contact</li>
 </ul>
     </header>
-
+<section className='bg-fuchsia-950 h-96'>
+    <h3>Coder</h3>
+</section>
     </>
   )
 }
